2ec29f8b1bddd7087b8fa11d616113f92f0f2934
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-30 20:40:05 +0100

    styling

--- App.js
+++ App.js
@@ -7,11 +7,6 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Open up App.js to start working on your application as another thing!
-        </Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
         <Landing />
       </View>
     );
@@ -21,7 +16,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center'
   }
