f306512fb32318c4c63d12a042e92f378e3b5266
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-30 20:39:50 +0100

    added textinput

--- components/Landing/Landing.js
+++ components/Landing/Landing.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class Landing extends Component {
+  constructor() {
+    super();
+    this.state = { textValue: '' };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Landing</Text>
+        <TextInput
+          placeholder="Enter"
+          value={this.state.textValue}
+          onChangeText={text => this.setState({ textValue: text })}
+        />
       </View>
     );
   }
