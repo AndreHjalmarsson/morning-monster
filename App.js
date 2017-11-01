2f8c086d8f0787359cf1361d4aa65c89f351d90e
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-01 10:33:51 +0100

    added swiper

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Landing />
-      </View>
+      <Swiper loop={false} showsPagination={false} index={0}>
+        <View style={styles.container}>
+          <Landing />
+        </View>
+        <Swiper
+          horizontal={false}
+          loop={false}
+          showsPagination={false}
+          index={1}
+        >
+          <View style={styles.container}>
+            <Home />
+          </View>
+        </Swiper>
+      </Swiper>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   }
 });
