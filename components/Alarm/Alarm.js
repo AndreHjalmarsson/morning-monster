8fb59e69b53afb45532942668e26b56026c93557
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-03 09:47:59 +0100

    added range slider

--- components/Alarm/Alarm.js
+++ components/Alarm/Alarm.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Slider } from 'react-native';
+import PropTypes from 'prop-types';
+import CircularSlider from 'react-native-circular-slider';
 
 export default class Alarm extends Component {
+  constructor() {
+    super();
+    this.state = { startAngle: 0, angleLength: 2 };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Alarm</Text>
+        <Text>Alarmm</Text>
+        <CircularSlider
+          startAngle={this.state.startAngle}
+          angleLength={this.state.angleLength}
+          onUpdate={({ startAngle, angleLength }) =>
+            this.setState({ startAngle, angleLength })}
+          segments={5}
+          strokeWidth={40}
+          radius={145}
+          gradientColorFrom="#ff9800"
+          gradientColorTo="#ffcf00"
+          showClockFace
+          clockFaceColor="#9d9d9d"
+          bgCircleColor="#171717"
+        />
       </View>
     );
   }
@@ -16,5 +37,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  slider: {
+    width: 200
   }
 });
