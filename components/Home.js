f6dfdaf1234e86c8ca41c7f68a50e92e9f1a8847
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-04 19:18:47 +0100

    added styling

---
+++ components/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+import Alarm from './Alarm';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Swiper loop={false} showsPagination={false} index={0}>
+        <View style={styles.container}>
+          <Text>Home</Text>
+        </View>
+        <Swiper
+          horizontal={false}
+          loop={false}
+          showsPagination={false}
+          scrollEnabled={false}
+        >
+          <View style={styles.container}>
+            <Alarm />
+          </View>
+        </Swiper>
+      </Swiper>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
