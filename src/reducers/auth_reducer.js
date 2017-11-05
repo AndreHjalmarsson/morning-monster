2fe87a084640ca9245ce17b5835c1b4ca4596f5c
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-05 17:28:24 +0100

    logout added

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,9 +1,11 @@
-import { AUTH_USER } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER } from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case AUTH_USER:
       return { ...state, authed: true };
+    case UNAUTH_USER:
+      return { ...state, authed: false };
   }
   return state;
 }
