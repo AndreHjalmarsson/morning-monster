ad3b3e40113b4b5a68304e63c15e831f2d76ce51
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-05 14:53:43 +0100

    rearranged root

--- App.js
+++ App.js
@@ -1,50 +1,9 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import Swiper from 'react-native-swiper';
-import firebase from 'firebase';
 
-import Landing from './src/components/Landing';
-import Home from './src/components/Home';
+import Root from './src/Root';
 
 export default class App extends Component {
-  state = { loggedIn: false };
-
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAqBrGHxkelJc8Dgxk-wdPUTyIrWHkAvgk',
-      authDomain: 'morning-monster.firebaseapp.com',
-      databaseURL: 'https://morning-monster.firebaseio.com',
-      projectId: 'morning-monster',
-      storageBucket: 'morning-monster.appspot.com',
-      messagingSenderId: '423602998161'
-    });
-
-    firebase.auth().onAuthStateChanged(user => {
-      user
-        ? this.setState({ loggedIn: true })
-        : this.setState({ loggedIn: false });
-    });
-  }
-
-  logoutUser() {
-    this.setState({ loggedIn: false });
-  }
-
-  renderHome() {
-    if (this.state.loggedIn) {
-      return <Home logoutUser={this.logoutUser.bind(this)} />;
-    } else {
-      return <Landing />;
-    }
-  }
-
   render() {
-    return <View style={styles.container}>{this.renderHome()}</View>;
+    return <Root />;
   }
 }
-
-const styles = {
-  container: {
-    flex: 1
-  }
-};
