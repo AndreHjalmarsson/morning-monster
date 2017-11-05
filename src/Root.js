652313bab8b072632fa1a4d926e1bd84f778ed5b
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-05 16:57:58 +0100

    root added

--- src/Root.js
+++ src/Root.js
@@ -1,58 +1,33 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import Swiper from 'react-native-swiper';
-import firebase from 'firebase';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from './actions';
 
-import reducers from './reducers';
 import Landing from './components/Landing';
 import Home from './components/Home';
 
 export default class Root extends Component {
-  constructor() {
-    super();
-
-    this.state = { loggedIn: false };
-  }
-
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAqBrGHxkelJc8Dgxk-wdPUTyIrWHkAvgk',
-      authDomain: 'morning-monster.firebaseapp.com',
-      databaseURL: 'https://morning-monster.firebaseio.com',
-      projectId: 'morning-monster',
-      storageBucket: 'morning-monster.appspot.com',
-      messagingSenderId: '423602998161'
-    });
-
-    firebase.auth().onAuthStateChanged(user => {
-      user
-        ? this.setState({ loggedIn: true })
-        : this.setState({ loggedIn: false });
-    });
-  }
-
-  logoutUser() {
-    this.setState({ loggedIn: false });
-  }
-
   renderHome() {
-    if (this.state.loggedIn) {
-      return <Home logoutUser={this.logoutUser.bind(this)} />;
+    if (this.props.auth) {
+      return <Home />;
     } else {
       return <Landing />;
     }
   }
 
   render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <View style={styles.container}>{this.renderHome()}</View>
-      </Provider>
-    );
+    return <View style={styles.container}>{this.renderHome()}</View>;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth.authed
+  };
+}
+
+connect(mapStateToProps, actionCreators)(Root);
 
 const styles = {
   container: {
