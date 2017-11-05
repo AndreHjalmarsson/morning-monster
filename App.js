441c5902e7e156daa97646e8f8f3ea2a56ce6056
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-05 12:44:00 +0100

    fixed nav bug

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import Swiper from 'react-native-swiper';
 import firebase from 'firebase';
 
-import Landing from './components/Landing';
-import Home from './components/Home';
+import Landing from './src/components/Landing';
+import Home from './src/components/Home';
 
 export default class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAqBrGHxkelJc8Dgxk-wdPUTyIrWHkAvgk',
@@ -16,26 +18,28 @@ export default class App extends Component {
       storageBucket: 'morning-monster.appspot.com',
       messagingSenderId: '423602998161'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      user
+        ? this.setState({ loggedIn: true })
+        : this.setState({ loggedIn: false });
+    });
+  }
+
+  logoutUser() {
+    this.setState({ loggedIn: false });
+  }
+
+  renderHome() {
+    if (this.state.loggedIn) {
+      return <Home logoutUser={this.logoutUser.bind(this)} />;
+    } else {
+      return <Landing />;
+    }
   }
 
   render() {
-    return (
-      <Swiper loop={false} showsPagination={false} index={0}>
-        <View style={styles.container}>
-          <Landing />
-        </View>
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={1}
-        >
-          <View style={styles.container}>
-            <Home />
-          </View>
-        </Swiper>
-      </Swiper>
-    );
+    return <View style={styles.container}>{this.renderHome()}</View>;
   }
 }
 
