f079ff09ce3e1bd7f007267c14838a1dffec39a0
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-07 21:14:12 +0100

    more on fething alarm time

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './auth_reducer';
+import alarmReducer from './alarm_reducer';
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  alarm: alarmReducer
 });
 
 export default rootReducer;
