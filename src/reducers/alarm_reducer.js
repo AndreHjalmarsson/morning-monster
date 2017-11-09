24384945a03890865223819690b887fb646f917b
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-09 13:36:15 +0100

    more on alarm

--- src/reducers/alarm_reducer.js
+++ src/reducers/alarm_reducer.js
@@ -1,9 +1,11 @@
-import { ALARM_FETCH } from '../actions/types';
+import { ALARM_FETCH, ALARM_ACTIVATE } from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case ALARM_FETCH:
       return { ...state, alarmTime: action.payload };
+    case ALARM_ACTIVATE:
+      return { ...state, active: true };
   }
   return state;
 }
