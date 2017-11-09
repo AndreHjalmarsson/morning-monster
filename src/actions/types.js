b6db6c6ffa39429d9a9f8775ca283b987acc835c
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-09 13:36:09 +0100

    added types

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,4 @@ export const UNAUTH_USER = 'unauth_user';
 export const LOADING = 'loading';
 export const STOP_LOADING = 'stop_loading';
 export const ALARM_FETCH = 'alarm_fetch';
+export const ALARM_ACTIVATE = 'alarm_activate';
