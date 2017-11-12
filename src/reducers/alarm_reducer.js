63a34107568faa175b52d33921d6ebf1c2d0af0c
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-12 16:50:09 +0100

    inactive red added

--- src/reducers/alarm_reducer.js
+++ src/reducers/alarm_reducer.js
@@ -1,4 +1,10 @@
-import { ALARM_FETCH, ALARM_ACTIVATE } from '../actions/types';
+import {
+  ALARM_FETCH,
+  ALARM_ACTIVATE,
+  ALARM_INACTIVATE,
+  ALARM_ON,
+  ALARM_OFF
+} from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
@@ -6,6 +12,12 @@ export default function(state = {}, action) {
       return { ...state, alarmTime: action.payload };
     case ALARM_ACTIVATE:
       return { ...state, active: true };
+    case ALARM_INACTIVATE:
+      return { ...state, active: false };
+    case ALARM_ON:
+      return { ...state, on: true };
+    case ALARM_OFF:
+      return { ...state, on: false };
   }
   return state;
 }
