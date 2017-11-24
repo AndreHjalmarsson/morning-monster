e3ca8393f0d18b159c1213315b90671ec92fc639
André Hjalmarsson <andre@Andres-MacBook-Pro.local>
2017-11-24 10:09:28 +0100

    added ar scene

--- src/components/ARScene.js
+++ src/components/ARScene.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  PixelRatio,
+  TouchableHighlight
+} from 'react-native';
 import { connect } from 'react-redux';
+import { ViroSceneNavigator, ViroARSceneNavigator } from 'react-viro';
 import * as actionCreators from '../actions';
 
+let InitialARScene = require('../../js/HelloWorldSceneAR');
+
+let sharedProps = {
+  apiKey: '74544290-1284-420A-B9BC-54DE4C1D5461'
+};
+
 class ARScene extends Component {
+  constructor() {
+    super();
+
+    this.state = { sharedProps: sharedProps };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Button title="Catch!!!" onPress={() => this.props.stopAlarm()} />
-      </View>
+      <ViroARSceneNavigator
+        {...this.state.sharedProps}
+        initialScene={{ scene: InitialARScene }}
+      />
     );
   }
 }
