5b788db0455013b35b5a8e356f23f427db63b83d
André Hjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-26 11:11:10 +0100

    background comp added

---
+++ src/components/common/BackgroundImage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Image } from 'react-native';
+
+class BackgroundImage extends Component {
+  render() {
+    return (
+      <View style={style.imageWrapper}>
+        <Image
+          style={style.imageStyle}
+          source={require('../../../img/bg-pink.jpg')}
+        />
+      </View>
+    );
+  }
+}
+
+let style = {
+  imageWrapper: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
+  },
+  imageStyle: {
+    flex: 1
+  }
+};
+
+export { BackgroundImage };
