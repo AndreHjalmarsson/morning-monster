94493c8159cbc7b9ef416c4a309fa81e841fbbcc
André Hjalmarsson <andre.hjalmarsson1@gmail.com>
2017-11-28 13:34:21 +0100

    changed dev settings

--- ios/morningmonster/AppDelegate.m
+++ ios/morningmonster/AppDelegate.m
@@ -37,7 +37,9 @@
                                                       moduleName:@"morningmonster"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:0.99 green:0.34 blue:0.54 alpha:1];
+    // rootView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg-pink--gradient.png"]];
+    // rootView.backgroundColor = [UIColor clearColor];
 
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     UIViewController *rootViewController = [UIViewController new];
