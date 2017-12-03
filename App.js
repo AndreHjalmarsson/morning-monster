7a0c791a25e5482063eefe7efc64292b1e741be5
André Hjalmarsson <andre.hjalmarsson1@gmail.com>
2017-12-03 13:11:55 +0100

    changes to proj structure

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -11,17 +10,6 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
 
 export default class App extends Component {
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAqBrGHxkelJc8Dgxk-wdPUTyIrWHkAvgk',
-      authDomain: 'morning-monster.firebaseapp.com',
-      databaseURL: 'https://morning-monster.firebaseio.com',
-      projectId: 'morning-monster',
-      storageBucket: 'morning-monster.appspot.com',
-      messagingSenderId: '423602998161'
-    });
-  }
-
   render() {
     return (
       <Provider store={store}>
