e93fddcf1840a605bae214c22c52e555093bd8f8
André Hjalmarsson <andre.hjalmarsson1@gmail.com>
2017-12-04 11:54:11 +0100

    fixed proper import

--- src/components/ARScene.js
+++ src/components/ARScene.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { View, Button, PixelRatio, TouchableHighlight } from 'react-native';
 import { connect } from 'react-redux';
 import { ViroSceneNavigator, ViroARSceneNavigator } from 'react-viro';
-import * as actionCreators from '../actions';
 
-let InitialARScene = require('../../js/HelloWorldSceneAR');
+import * as actionCreators from '../actions';
+import InitialARScene from '../../js/HelloWorldSceneAR';
 
 let sharedProps = {
   apiKey: '74544290-1284-420A-B9BC-54DE4C1D5461'
